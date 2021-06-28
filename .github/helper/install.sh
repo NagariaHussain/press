67ebfa7833e4b20e0a710e095d0d9771af4a0b80
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-06-28 11:38:42 +0530

    ci: Mariadb configuration

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -12,30 +12,18 @@ mkdir ~/frappe-bench/sites/test_site
 cp ${GITHUB_WORKSPACE}/.travis/mariadb.json ~/frappe-bench/sites/test_site/site_config.json
 
 
-# mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'";
-# mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
-
-# mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe_consumer";
-# mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe_consumer'@'localhost' IDENTIFIED BY 'test_frappe_consumer'";
-# mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe_consumer\`.* TO 'test_frappe_consumer'@'localhost'";
-
-# mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe_producer";
-# mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe_producer'@'localhost' IDENTIFIED BY 'test_frappe_producer'";
-# mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe_producer\`.* TO 'test_frappe_producer'@'localhost'";
-
-# mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'";
-# mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES";
-
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'";
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'";
 
 mysql --host 127.0.0.1 --port 3306 -u root -e "DROP DATABASE IF EXISTS test_frappe";
 mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe";
+
 mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'";
 mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'";
-
 mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'";
+
 mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES";
+
 
 cd ./frappe-bench || exit
 
